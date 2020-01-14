2e3a993fa99fe9bafc58333d1bf0c76c7ab752fd
Andreas (AK) <andreas@typedigital.de>
2020-01-14 09:27:58 +0100

    fix: linted rules

--- base.js
+++ base.js
@@ -2,79 +2,79 @@
 /* eslint-disable sort-keys */
 
 module.exports = {
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   env: {
     es6: true,
   },
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: "module",
+    sourceType: 'module',
   },
   extends: [
-    "plugin:jest/recommended",
-    "airbnb-typescript"
+    'plugin:jest/recommended',
+    'airbnb-typescript',
   ],
   plugins: [
-    "@typescript-eslint/eslint-plugin",
-    "import",
-    "react",
-    "jest"
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'react',
+    'jest',
   ],
   rules: {
-    "linebreak-style": [
-      "off"
+    'linebreak-style': [
+      'off',
     ],
-    "indent": 0,
-    "@typescript-eslint/indent": [
-      "error",
-      2
-    ],
-    "max-len": [
+    'indent': 0,
+    '@typescript-eslint/indent': [
+      'error',
       2,
-      120
     ],
-    "import/no-extraneous-dependencies": [
-      "error",
+    'max-len': [
+      2,
+      120,
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        "peerDependencies": true,
-        "devDependencies": [
-          "mocks/**/*",
-          "src/stories/**",
-          "src/setupTests.ts",
-          "__tests__/**/*",
-          "jest/**/*",
-          "**/**/*.story.tsx"
-        ]
-      }
+        'peerDependencies': true,
+        'devDependencies': [
+          'mocks/**/*',
+          'src/stories/**',
+          'src/setupTests.ts',
+          '__tests__/**/*',
+          'jest/**/*',
+          '**/**/*.story.tsx',
+        ],
+      },
     ],
-    "import/prefer-default-export": 1,
-    "import/no-cycle": [
+    'import/prefer-default-export': 1,
+    'import/no-cycle': [
       1,
       {
-        "maxDepth": 7
-      }
+        'maxDepth': 7,
+      },
     ],
-    "react/jsx-props-no-spreading": 1,
-    "consistent-return": 0,
-    "react/sort-comp": 1,
-    "react/no-array-index-key": 1,
-    "react/prop-types": 0,
-    "react/destructuring-assignment": 1,
-    "jest/consistent-test-it": 2,
-    "jest/expect-expect": 2,
-    "jest/no-duplicate-hooks": 2,
-    "jest/no-export": 2,
-    "jest/no-identical-title": 2,
-    "jest/no-if": 2,
-    "jest/no-jasmine-globals": 2,
-    "jest/no-jest-import": 2,
-    "jest/no-test-callback": 2,
-    "jest/prefer-expect-assertions": 2,
-    "jest/prefer-hooks-on-top": 2,
-    "jest/prefer-spy-on": 2
+    'react/jsx-props-no-spreading': 1,
+    'consistent-return': 0,
+    'react/sort-comp': 1,
+    'react/no-array-index-key': 1,
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 1,
+    'jest/consistent-test-it': 2,
+    'jest/expect-expect': 2,
+    'jest/no-duplicate-hooks': 2,
+    'jest/no-export': 2,
+    'jest/no-identical-title': 2,
+    'jest/no-if': 2,
+    'jest/no-jasmine-globals': 2,
+    'jest/no-jest-import': 2,
+    'jest/no-test-callback': 2,
+    'jest/prefer-expect-assertions': 2,
+    'jest/prefer-hooks-on-top': 2,
+    'jest/prefer-spy-on': 2,
   },
   globals: {
-    "window": true,
-    "jest/globals": true
-  }
-}
+    'window': true,
+    'jest/globals': true,
+  },
+};
