a5e20bd1b94dad508ce1f2f132fd9f4031ba116b
Andreas (AK) <andreas@typedigital.de>
2020-02-20 09:40:33 +0100

    reafctor!: updated base rule set base rule set is not longer only for the api

--- base.js
+++ base.js
@@ -1,3 +1,44 @@
-const config = require('./api.js')
+/* eslint sort-keys: ["error", "asc"], quote-props: ["error", "consistent"] */
+/* eslint-disable sort-keys */
 
-module.exports = config
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-typescript/base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+  ],
+  env: {
+    'node': true,
+  },
+  rules: {
+    'linebreak-style': [
+      'off',
+    ],
+    'indent': 0,
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+    ],
+    'max-len': [
+      2,
+      100,
+    ],
+    '@typescript-eslint/explicit-function-return-type': 1,
+    'no-plusplus': 1,
+    'consistent-return': 1,
+    'import/no-cycle': [
+      1,
+      {
+        'maxDepth': 5,
+      },
+    ],
+    'import/no-named-as-default': 1,
+  },
+};
