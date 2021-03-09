59526ac3c496fcbdc5de50b46062bbaaa162a789
Daniel Birke <daniel@bke-consulting.com>
2021-03-09 09:46:59 +0100

    major update

--- api.js
+++ api.js
@@ -4,6 +4,7 @@
 module.exports = {
   extends: [
     'plugin:mocha/recommended',
+    'airbnb-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
---
+++ nextjs.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  // ignore configs in other directories
+  root: true,
+  globals: {
+    React: 'writable',
+  },
+  rules: {
+    'require-default-props': 'off',
+    "linebreak-style": ["off"],
+    "indent": 0,
+    "react/jsx-indent": 0,
+    "@typescript-eslint/indent": ["error", 2],
+    "max-len": [2, 120],
+    "react/prop-types": 0,
+    "jsx-a11y/anchor-is-valid":0,
+    "func-style": ["error", "declaration", { "allowArrowFunctions": true }]
+  }
+};
