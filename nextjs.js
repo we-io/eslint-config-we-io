7c172b7ae49431f379138ce85fa7acd04d88855b
Daniel Birke <daniel@bke-consulting.com>
2021-09-21 19:23:54 +0200

    updated config

--- nextjs.js
+++ nextjs.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'airbnb',
     'airbnb-typescript',
-    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
