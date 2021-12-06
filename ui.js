702f26e84195d6860e5589b047e997d134547e85
Daniel Birke <daniel@bke-consulting.com>
2021-12-06 11:49:05 +0100

    added jest plugin

--- ui.js
+++ ui.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+  plugins: ['jest'],
   rules: {
     'linebreak-style': ['off'],
     indent: 0,
